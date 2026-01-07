3c608def252772dd19e977f0918b7bfd365a4d29
Jason <jsfernandes83@gmail.com>
2026-01-06 23:29:45 -0700

    chore: initial nuxt app with vuetify commit

---
+++ app/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-app>
+      <NuxtRouteAnnouncer />
+      <v-main>
+        <v-container>
+          <!-- Toggle between Light / Dark -->
+          <v-btn @click="theme.toggle()" text="Toggle Light / Dark"></v-btn>
+
+          <!-- Change to a specific theme -->
+          <v-btn @click="theme.change('dark')" text="Change to Dark"></v-btn>
+
+          <!-- Cycle between all themes -->
+          <v-btn @click="theme.cycle()" text="Cycle All Themes"></v-btn>
+
+          <!-- Cycle between specific themes -->
+          <v-btn
+            @click="theme.cycle(['custom', 'light', 'system'])"
+            text="Cycle Specific Themes"
+          ></v-btn>
+        </v-container>
+      </v-main>
+      <NuxtWelcome />
+    </v-app>
+  </div>
+</template>
+
+<script setup type="ts">
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+</script>
